999126ce89e909147ca7f30c2e05fb8c89f85cc4
Will Junkin <williamjjunkin@gmail.com>
2022-08-22 14:05:14 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+function mondayWork(task = 'go to the office'){
+    return `This Monday, I will ${task}.`
+}
+function wrapAdjective(word = '*'){
+    const innerFunction = function (adjective = 'special'){
+        return `You are ${word}${adjective}${word}!`    
+    }
+    return innerFunction
+}
